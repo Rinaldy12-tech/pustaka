42cacd1259a7ea3a1efdce70254bf2791bcd1f73
Rinaldy12-tech <rinaldy.faishal@gmail.com>
2024-05-02 09:26:20 +0700

    Menambahkan Contoh Template CI

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -1,33 +1,33 @@
-
 <?php
-
-class Matakuliah extends CI_Controller 
+class Matakuliah extends CI_Controller
 {
-    Public function index()
+    public function index()
+
     {
         $this->load->view('view-form-matakuliah');
-        $this->load->helper('url');
+        $this->load->library('form-validation');
     }
 
-    Public function cetak()
+    public function cetak()
     {
-       $this->form_validation->set_rules('kode', 'nama', 'required|min_length[3]', [
-            'required' => 'Kode Matakuliah Harus Diisi',
-            'min_lenght' => 'Kode terlalu pendek'
-        ]);
-
-        if ($this->form_validation->run() != true) {
-            $this->load->view('view-form-matakuliah');
-        } else {
-            
-        $data = [
+        $data =[
             'kode' => $this->input->post('kode'),
             'nama' => $this->input->post('nama'),
-            'sks'  => $this->input->post('sks')
+            'sks' => $this->input->post('sks')
         ];
-        
+
         $this->load->view('view-data-matakuliah', $data);
-        }
+    }
+
+    public function rules()
+    {
+        return[
+        [
+            'field' => 'kode',
+            'label' => 'Kode',
+            'rules' => 'required|max_length[16]|numeric'
+        ]
+
+        ];
     }
 }
-?>
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1>
+        <?php echo $judul ?>
+    </h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rinaldy</li>
+        <li>Nama Belakang   : Faishal</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Griya Asri Bahagia , Babelan Bekasi</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Wonogiri</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
